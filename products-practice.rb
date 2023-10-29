8eea9cd6bf97d9b153b992f209f5781a9153272f
marioalija <marioalija25@gmail.com>
2023-10-28 23:56:37 -0500

    -m

--- products-practice.rb
+++ products-practice.rb
@@ -2,3 +2,7 @@ require "http"
 response = HTTP.get("http://localhost:3000/products.json")
 data = response.parse
 pp data
+require "http"
+response = HTTP.get("http://localhost:3000/all_products.json")
+data = response.parse
+pp data
